1c61043b161fa7684d4624deb7dd0ecf73c5510f
evil <eduardo.valdivia@tecsanpedro.edu.mx>
2019-10-23 09:54:02 -0500

    c++

---
+++ primeros_programa/holaMundo/hola.cpp
@@ -0,0 +1,15 @@
+/*
+  Primer programa en C++
+*/
+
+
+#include<iostream> //Biblioteca para funciones cout cin
+
+using namespace std;
+
+int main(){
+
+  std::cout<< "Hola Mundo!!!";
+  return 1;
+  
+}
